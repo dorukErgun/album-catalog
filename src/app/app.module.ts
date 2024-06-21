fa23dec971fffb87ff6a0da9e19ae59a24e6eb18
Ergun <doruk310121@gmail.com>
2024-06-21 17:51:11 +0300

    Add tooltipts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { NgOptimizedImage } from '@angular/common';
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/shared/card/card.component.ts
+++ src/app/components/shared/card/card.component.ts
@@ -9,4 +9,5 @@ export class CardComponent {
   @Input() image: string = "";
   @Input() title: string = "";
   @Input() description: string = "";
+  @Input() tooltipMessage: string = "";
 }
